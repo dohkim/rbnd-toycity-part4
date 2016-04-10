af64c47dfeb25d9256744122f4465912f47934c7
Doh <tank2705@gmail.com>
2016-04-10 07:37:41 +0000

    save before applying statis variable

--- data/seeds.rb
+++ data/seeds.rb
@@ -2,7 +2,14 @@ require 'faker'
 
 # This file contains code that populates the database with
 # fake data for testing purposes
-
 def db_seed
-  # Your code goes here!
+    # Your code goes here!
+  10.times do
+    brand = Faker::Company.name
+    name = Faker::Commerce.product_name
+    price = Faker::Commerce.price
+    Product.create(brand: brand, name: name, price: price)
+  end
+  
+  
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,71 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  $data_path = File.dirname(__FILE__) + "/../data/data.csv"
+  
+  
+  def self.create(opts={})
+    new_record = self.new(opts)
+    new_record.save
+    p new_record
+  end
+  
+  def save
+    CSV.open($data_path, "ab") do |csv|
+      csv << [self.id, self.brand, self.name, self.price]
+    end
+  end
+  
+  def self.all
+    list=[]
+    CSV.read($data_path).drop(1).each do |row|
+      list << self.new(id: row[0], brand: row[1], name: row[2], price: row[3])
+    end
+    p list
+  end
+  
+  def self.first(n=1)
+    list=self.all
+    p list.first(n)
+    if n==1
+      p list.first
+    else
+      p list.first(n)
+    end
+  end
+  
+  def self.last(n=1)
+      list=self.all
+    if n==1
+      p list.last
+    else
+      p list.last(n)
+    end
+  end
+  
+  def self.find(n)
+    list=self.all
+    p list[n-1]
+  end
+  
+  def self.destroy(n)
+    remove_product=self.find(n)
+    self.remove_product_in_db(n)
+    p remove_product
+  end
+  
+  def self.remove_product_in_db(n)
+    table=CSV.table($data_path)
+    table.delete_if do |row|
+      row[:id]==n
+    end
+    File.open($data_path, 'w') do |f|
+      f.write(table.to_csv)
+    end
+  end
+  
+  
+  def self.method_missing(method_name, *arguments, &block)
+  end
+  
 end
